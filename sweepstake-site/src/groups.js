3347973bf7721352ae64c35084bbf6323080da2e
PortsDayOne <portsdayone@gmail.com>
2026-06-21 08:47:49 +0100

    Update groups.js

--- sweepstake-site/src/groups.js
+++ sweepstake-site/src/groups.js
@@ -12,12 +12,10 @@ export const OWNERS = {
   "Portugal": "Darrell", "DR Congo": "Joe", "Colombia": "Joanne", "Uzbekistan": "Sam",
   "England": "Lottie", "Croatia": "Karina", "Ghana": "Darrell", "Panama": "Darrell",
 };
-
 export const PLAYER_COLORS = {
   Tom: "#F0C446", Lottie: "#40C6A0", Sam: "#FF9F43", Joanne: "#E0556E",
   Matt: "#C77DFF", Joe: "#5896FF", Darrell: "#9DB2BF", Karina: "#4ECDC4",
 };
-
 export const GROUPS = [
   { name: "Group A", teams: [
     { name: "Mexico",       gp:2, w:2, d:0, l:0, gf:3,  ga:0, pts:6 },
@@ -32,10 +30,10 @@ export const GROUPS = [
     { name: "Qatar",        gp:2, w:0, d:1, l:1, gf:1,  ga:7, pts:1 },
   ]},
   { name: "Group C", teams: [
-    { name: "Brazil",       gp:2, w:1, d:1, l:0, gf:4,  ga:1, pts:4 },
+    { name: "Brazil",       gp:3, w:2, d:1, l:0, gf:7,  ga:1, pts:7 },
     { name: "Morocco",      gp:2, w:1, d:1, l:0, gf:2,  ga:1, pts:4 },
     { name: "Scotland",     gp:2, w:1, d:0, l:1, gf:1,  ga:1, pts:3 },
-    { name: "Haiti",        gp:2, w:0, d:0, l:2, gf:0,  ga:4, pts:0 },
+    { name: "Haiti",        gp:3, w:0, d:0, l:3, gf:0,  ga:7, pts:0 },
   ]},
   { name: "Group D", teams: [
     { name: "USA",          gp:2, w:2, d:0, l:0, gf:6,  ga:1, pts:6 },
@@ -44,16 +42,16 @@ export const GROUPS = [
     { name: "Turkiye",      gp:2, w:0, d:0, l:2, gf:0,  ga:3, pts:0 },
   ]},
   { name: "Group E", teams: [
-    { name: "Germany",      gp:1, w:1, d:0, l:0, gf:7,  ga:1, pts:3 },
-    { name: "Ivory Coast",  gp:1, w:1, d:0, l:0, gf:1,  ga:0, pts:3 },
-    { name: "Ecuador",      gp:1, w:0, d:0, l:1, gf:0,  ga:1, pts:0 },
-    { name: "Curacao",      gp:1, w:0, d:0, l:1, gf:1,  ga:7, pts:0 },
+    { name: "Germany",      gp:2, w:2, d:0, l:0, gf:9,  ga:2, pts:6 },
+    { name: "Ivory Coast",  gp:2, w:1, d:0, l:1, gf:2,  ga:1, pts:3 },
+    { name: "Ecuador",      gp:2, w:0, d:1, l:1, gf:0,  ga:1, pts:1 },
+    { name: "Curacao",      gp:2, w:0, d:1, l:1, gf:1,  ga:7, pts:1 },
   ]},
   { name: "Group F", teams: [
-    { name: "Sweden",       gp:1, w:1, d:0, l:0, gf:5,  ga:1, pts:3 },
-    { name: "Netherlands",  gp:1, w:0, d:1, l:0, gf:2,  ga:2, pts:1 },
-    { name: "Japan",        gp:1, w:0, d:1, l:0, gf:2,  ga:2, pts:1 },
-    { name: "Tunisia",      gp:1, w:0, d:0, l:1, gf:1,  ga:5, pts:0 },
+    { name: "Netherlands",  gp:2, w:1, d:1, l:0, gf:7,  ga:3, pts:4 },
+    { name: "Japan",        gp:2, w:1, d:1, l:0, gf:6,  ga:2, pts:4 },
+    { name: "Sweden",       gp:2, w:1, d:0, l:1, gf:6,  ga:6, pts:3 },
+    { name: "Tunisia",      gp:2, w:0, d:0, l:2, gf:1,  ga:9, pts:0 },
   ]},
   { name: "Group G", teams: [
     { name: "Belgium",      gp:1, w:0, d:1, l:0, gf:1,  ga:1, pts:1 },
